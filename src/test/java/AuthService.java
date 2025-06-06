615ae3ae76a53bce73eed2c3581f6c9ec761803b
pritamhoro <pritam.horo143@gmail.com>
2025-06-06 23:10:18 +0530

    added test script to check githubAction

--- src/test/java/AuthService.java
+++ src/test/java/AuthService.java
@@ -1,4 +1,26 @@
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import java.util.*;
+
+
+import static io.restassured.RestAssured.*;
+
 public class AuthService
 {
+    @Test
+    public static void Platzi_Fake_Store_API()
+    {
+        Map<String,String> payload= new HashMap<String,String>();
+        payload.put("email","john@mail.com");
+        payload.put("password","changeme");
+        baseURI="https://api.escuelajs.co";
+        Response response = given()
+                .contentType(ContentType.JSON)
+                .body(payload)
+                .when()
+                .post("/api/v1/auth/login");
 
+    }
 }
